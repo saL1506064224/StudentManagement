78a3568a68b08b6476e1c13b09d2d1447dac2eae
oonaa <L1506064224as@gmail.com>
2024-07-17 15:55:45 +0900

    syokai

--- src/main/java/student/management/StudentManagement/StudentManagementApplication.java
+++ src/main/java/student/management/StudentManagement/StudentManagementApplication.java
@@ -1,21 +1,40 @@
 package student.management.StudentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private String name = "finfin";
+	private String age = "99";
+	private Map<String, String> map = new HashMap<>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello, World";
+	@GetMapping("/studentInfo")
+	public String getStudentInfo() {
+		String text = "";
+		for(Map.Entry<String, String> entry :map.entrySet()){
+			text += entry.getKey() + " " + entry.getValue() + "sai\n";
+		}
+		//text += map.forEach((k ,v) -> {text.join(k); });
+		return text;
+	}
+
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name, String age) {
+		this.name = name;
+		this.age = age;
+		map.put(name, age);
 	}
 
 }
